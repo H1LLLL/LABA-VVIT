1d5efd3930e6a57012717088d5252917fe56d85b
CHILLLL <vaskinandrey27@gmail.com>
2026-01-15 05:16:47 +0300

    Update 2.2.py

--- Laba2/2.2.py
+++ Laba2/2.2.py
@@ -1,109 +1,41 @@
-def main():
-    # 1.1
-    def greet(name):
-        return f"Привет, {name}!"
+name = str(input('Введите Имя:'))
+def greet(name):
+    print('привет', name)
+greet(name)
+number = int(input('Введите число: '))
+def square(number):
+    n = number**2
+    print('Число в квадрате: ', n)
+square(number)
+x = int(input('Первое число:'))
+y = int(input('Второе число:'))
 
-    name = input('Введите ваше имя (Или "стоп", чтобы остановить программу):')
-    if name.lower() == 'стоп':
-        print('Выход из программы')
-        return
-    print(greet(name))
-
-    # 1.2
-    def square(number):
-        return number ** 2
-
-    while True:
-        number = input('Введите ваше число (Или "стоп", чтобы остановить программу):')
-        if number.lower() == 'стоп':
-            print('Завершил прогрогрмму!')
-            return
-        try:
-            num = float(number)
-            print('Ваше число в квадрате:', square(num))
-            break
-        except ValueError:
-            print('Вы ввели не правильное число')
-
-    # 1.3
-    def max_of_two(x, y):
-        return x if x > y else y
-
-    while True:
-        x_input = input('Введите первое число (Или "стоп"): ')
-        if x_input.lower() == 'стоп':
-            print('Завершил программу!')
-            return
-        y_input = input('Введите второе число (Или "стоп"): ')
-        if y_input.lower() == 'стоп':
-            print('Завершил программу!')
-            return
-        try:
-            x = float(x_input)
-            y = float(y_input)
-            print('Большее число:', max_of_two(x, y))
-            break
-        except ValueError:
-            print('Вы ввели не число, попробуйте снова.')
-
-
-main()
-
-
-# 2
-
-def describe_person(name, age=30):
-    print(f'Имя: {name}, Возраст: {age}')
-
-
-def main():
-    while True:
-        name = input('Введите свое имя (Или "стоп" для выхода): ')
-        if name.lower() == 'стоп':
-            print('Программа офнулась :)')
-            return
-
-        age_input = input('Введите ваш возраст (не обязательно, Enter = 30): ')
-        if age_input.lower() == 'стоп':
-            print('Программа офнулась :)')
-            return
-
-        if age_input == '':
-            describe_person(name)
-        else:
-            try:
-                age = int(age_input)
-                describe_person(name, age)
-            except ValueError:
-                print('Введите пожалуйста число не слово!')
-                continue
-
-
-main()
-
-
-# 3
-
+def max_of_two(x, y):
+    if x > y:
+        return print(x)
+    elif x < y:
+        return print(y)
+    else:
+        return print(x)
+max_of_two(x, y)
+name = str(input('Введите ваше имя: '))
+age = int(input('Введите ваш возраст (Если не хотите указывать возраст, напишите 0): '))
+def describe_person(name, age):
+    if age == 0:
+        return print('Вас зовут', name, 'вам', age+30)
+    elif age > 0:
+        return print('Вас зовут', name, 'вам', age)
+    else:
+        return print('Вы неправильно ввели свой возраст')
+describe_person(name, age)
+number = int(input('Введите простое число: '))
 def is_prime(number):
-    if number < 2:
-        print(f'{number} - сложное число')
-        return False
     for i in range(2, number):
         if number % i == 0:
-            print(f'{number} - сложное число')
             return False
-    print(f"{number} - простое число")
-    return True
-
-
-while True:
-    number = input('Введите ваше число для проверки простого числа(Или стоп для остановки):')
-    if number.lower() == 'стоп':
-        print('Прога пока')
-        break
-    try:
-        num = int(number)
-        is_prime(num)
-        print('Ваше число целое!')
-    except ValueError:
-        print('Ваше число не целое!!!')
+        else:
+            return True
+if is_prime(number) == True:
+    print('Число', number ,'- простое')
+else:
+    print('Число', number, '- не простое')
