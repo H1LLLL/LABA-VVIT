c8c27334b4b39bdf33660b84b20c0f397815a49b
CHILLLL <vaskinandrey27@gmail.com>
2026-01-15 04:59:41 +0300

    Update 1.1.py

--- Laba1/1.1.py
+++ Laba1/1.1.py
@@ -1,18 +1,21 @@
 try:
-    a = int(input())
-    for i in range(a):
-        print(i + 1)
-except ValueError:
-    print("Некорректные данные")
-try:
-    b = int(input())
-    c = int(input())
-    if b > c:
-        print(b)
-    elif b < c:
-        print(c)
+    a = int(input('Введите число:'))
+    if a>=1:
+        for i in range(a):
+            print(i+1)
     else:
-        print('=')
+        print('Введите положительное число')
 except ValueError:
-    print("Некорректные данные")
-gi
+    print('Введите число, а не символы')
+try:
+    print('Введите два целых числа:')
+    x = int(input())
+    y = int(input())
+    if x>y:
+        print(x)
+    elif x<y:
+        print(y)
+    else:
+        print('Числа равны')
+except ValueError:
+    print('Введите два числа')
